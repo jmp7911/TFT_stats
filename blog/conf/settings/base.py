3549053f28061e37dffd413b62ea5cadde3a7f96
JiMan Park <jmp7911@gmail.com>
2024-02-05 16:32:39 +0900

    fix: register_serializer dj_rest_auth 로 변경

--- blog/conf/settings/base.py
+++ blog/conf/settings/base.py
@@ -73,7 +73,7 @@ REST_AUTH = {
     'PASSWORD_RESET_CONFIRM_SERIALIZER': 'dj_rest_auth.serializers.PasswordResetConfirmSerializer',
     'PASSWORD_CHANGE_SERIALIZER': 'dj_rest_auth.serializers.PasswordChangeSerializer',
 
-    'REGISTER_SERIALIZER': 'accounts.serializers.UserSerializer',
+    'REGISTER_SERIALIZER': 'dj_rest_auth.registration.serializers.RegisterSerializer',
 
     'REGISTER_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
 
