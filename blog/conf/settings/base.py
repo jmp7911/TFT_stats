6f7b8f4c42eb3a9f50268f0b20b08746e299399a
JiMan Park <jmp7911@gmail.com>
2024-03-20 20:42:12 +0900

    settings.py STASTIC_ROOT 추가

--- blog/conf/settings/base.py
+++ blog/conf/settings/base.py
@@ -197,8 +197,10 @@ LOCALE_PATHS = [BASE_DIR / 'locale']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
 # LOGIN_URL = '/accounts/login/'
 
 MEDIA_URL = '/media/'
