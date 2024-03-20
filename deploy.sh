df7245d7e07759bbb4aa57471fb8dac1412ff915
JiMan Park <jmp7911@gmail.com>
2024-03-20 19:31:52 +0900

    CD] 서버 스크립트 실행

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+set -e
+
+# 이동
+cd blog/
+
+# Git Update
+git pull origin main --ff-only
+
+# .env 파일 생성
+echo DJANGO_SECRET_KEY=${{ secrets.DJANGO_SECRET_KEY }} >> .env
+echo DJANGO_SETTINGS_MODULE=${{ secrets.DJANGO_SETTINGS_MODULE }} >> .env
+echo DEBUG=${{ secrets.DEBUG }} >> .env
+echo RIOT_API_DEVELOPMENT_KEY=${{ secrets.RIOT_API_DEVELOPMENT_KEY }} >> .env
+echo RIOT_API_KEY=${{ secrets.RIOT_API_KEY }} >> .env
+echo SUMMONER_URL=${{ secrets.SUMMONER_URL }} >> .env
+
+# 모듈 설치
+pip install -r requirements.txt
+
+# DB 마이그레이션
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# 정적 파일 수집
+python3 manage.py collectstatic --noinput
+
+# gunicorn 재실행
+# sudo systemctl restart gunicorn.service
