0cb76ca736669e436cb4f110ac2a969354610909
JiMan Park <jmp7911@gmail.com>
2024-03-20 20:19:10 +0900

    CD] env 파일 생성 관리

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-set -e
-
-# .env 파일 생성
-echo DJANGO_SECRET_KEY=${{ secrets.DJANGO_SECRET_KEY }} > .env
-echo DJANGO_SETTINGS_MODULE=${{ secrets.DJANGO_SETTINGS_MODULE }} >> .env
-echo DEBUG=${{ secrets.DEBUG }} >> .env
-echo RIOT_API_DEVELOPMENT_KEY=${{ secrets.RIOT_API_DEVELOPMENT_KEY }} >> .env
-echo RIOT_API_KEY=${{ secrets.RIOT_API_KEY }} >> .env
-echo SUMMONER_URL=${{ secrets.SUMMONER_URL }} >> .env
 
 # 모듈 설치
 pip install -r requirements.txt
