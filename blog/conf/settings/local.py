0282814b055c19fafb723ab2d11fd94a7bbb55e1
JiMan Park <jmp7911@gmail.com>
2024-03-25 17:07:30 +0900

    google social auth 주석처리

--- blog/conf/settings/local.py
+++ blog/conf/settings/local.py
@@ -2,21 +2,3 @@ from .base import *
 
 
 ALLOWED_HOSTS = ['*']
-
-# Provider Configurations
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        "APP": {
-            "client_id": env("GOOGLE_CLIENT_ID"),
-            "secret": env("GOOGLE_SECRET_KEY"),
-            "key": ""
-        },
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        }
-    }
-}
