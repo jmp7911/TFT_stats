b538675b37a79705ef4d13b46443be66ca974f84
JiMan Park <jmp7911@gmail.com>
2024-04-29 15:30:36 +0900

    cors....

--- blog/conf/settings/prod.py
+++ blog/conf/settings/prod.py
@@ -1,11 +1,3 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.180.146.14', 'localhost']
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://54.180.146.14",
-    "http://54.180.146.14:8000",
-    "http://localhost",
-]
+ALLOWED_HOSTS = ['*']
